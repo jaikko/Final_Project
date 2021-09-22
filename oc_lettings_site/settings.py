c57d947fa90d4f0c337f27f1675ab3b5078d2463
jaikko <65661383+jaikko@users.noreply.github.com>
2021-09-22 15:37:20 +0200

    test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -2,10 +2,16 @@ import os
 import django_heroku
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+import environ
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Sentry
 sentry_sdk.init(
-    dsn="https://74dd60dbdb2c4eadb9a55de80a4855be@o1009246.ingest.sentry.io/5973815",
+    dsn=env('SENTRY_DSN'),
     integrations=[DjangoIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%
@@ -25,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 def trigger_error(request):
     division_by_zero = 1 / 0
+
 
 urlpatterns = [
     path('', views.index, name='index'),
