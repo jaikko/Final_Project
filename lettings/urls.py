587eb94600a24146f6283e60eefd55375d433ead
jaikko <65661383+jaikko@users.noreply.github.com>
2021-09-22 10:28:36 +0200

    test

--- lettings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'lettings'
-urlpatterns = [
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-]
